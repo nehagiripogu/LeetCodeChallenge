eca2c92635b510e8033008bf1cc184287f4543dd
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-05-21 22:29:02 +0530

    Time: 1 ms (71.5%), Space: 12.7 MB (10.23%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = list(s)
+
+        for i in range(0, len(s), 2 * k):
+
+            # reverse first k chars
+            s[i:i+k] = reversed(s[i:i+k])
+
+        return "".join(s)
