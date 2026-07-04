2ae67916329cc4de46eeb8ad9796cc241f8e9a3d
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-07-05 02:04:11 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -8,7 +8,7 @@ class Solution(object):
         subset=[]
         phone = {"2": "abc","3": "def","4": "ghi","5": "jkl","6": "mno","7": "pqrs","8": "tuv","9":"wxyz"}
         def backtrack(i,curstr):
-            if len(curstr)==len(digits):
+            if i==len(digits):
                 res.append(curstr)
                 return
             for c in phone[digits[i]]:
