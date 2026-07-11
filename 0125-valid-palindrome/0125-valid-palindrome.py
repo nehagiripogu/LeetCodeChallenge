0f70fe12865620837de9be62d3ab104f5e0c6cb6
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-07-11 23:58:43 +0530

    Time: 10 ms (36.99%), Space: 19.6 MB (76.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,18 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        l=0
-        r=len(s)-1
-        while l<r:
-            while l<r and not s[l].isalnum():
-                l+=1
-            while l<r and not s[r].isalnum():
-                r-=1
-            if s[l].lower()!=s[r].lower():
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.strip()
+        res=''
+        for s in s:
+            if s.isalnum():
+                res+=s
+        left=0
+        right=len(res)-1
+        res=res.lower()
+        if len(res)==1:
+            return True
+        while left<right:
+            if res[left]!=res[right]:
                 return False
-            l+=1
-            r-=1
+            left+=1
+            right-=1
         return True
