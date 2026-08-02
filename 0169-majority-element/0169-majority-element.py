8d92299fe0e418e51ed397561e137807fbb11b35
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-08-02 20:39:24 +0530

    Time: 6 ms (61.17%), Space: 21.2 MB (48.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,5 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        freq={}
-        for num in nums:
-            freq[num]=freq.get(num,0)+1
-            if freq[num]>(len(nums)//2):
-                return num
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq=Counter(nums)
+        
+        return freq.most_common()[0][0]
