9fb240700b1d214cb95f52526e648d7f3d6c274b
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-08-02 17:08:27 +0530

    Time: 5 ms (25.69%), Space: 20.3 MB (26.98%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -8,7 +8,6 @@ class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         q=deque([(root,0)])
         visited=set()
-        res=[]
         ans=0
         while q:
             level=[]
@@ -18,11 +17,7 @@ class Solution:
                 level.append(index)
                 if node.left:
                     q.append((node.left,2*index+1))
-                
                 if node.right:
                     q.append((node.right,2*index+2))
             ans=max(ans,level[-1]-level[0]+1)
-                
-            res.append(level)
-        
         return ans
